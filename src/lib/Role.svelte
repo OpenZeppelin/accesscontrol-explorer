e9e85d7843f9dbf0ea8eddb6b7361a81c63fbd37
Francisco Giordano <frangio.1@gmail.com>
2021-11-18 23:56:25 -0300

    add role dict

--- src/lib/Role.svelte
+++ src/lib/Role.svelte
@@ -3,12 +3,16 @@
 </script>
 
 <script lang="ts">
+  import roleDict from './role-dict.json';
+
   export let r: string;
   export let address: string;
 
   $: short = r.replace(/^0x(.{4}).*(.{4})$/, '0x$1...$2');
+
+  $: decoded = roleDict[r] ?? short;
 </script>
 
 <a href="/address/{address}/role/{r}" class="p-1 -m-1 rounded bg-gray-50 hover:bg-gray-100" title={r}>
-  {short}
+  {decoded}
 </a>
